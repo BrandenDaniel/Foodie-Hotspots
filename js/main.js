61da5921ad9d775208740aefe03cad7f72aeff0a
Branden Ng <brandendanielng@gmail.com>
2018-04-07 23:32:33 +1000

    Upload projects files

--- js/main.js
+++ js/main.js
@@ -148,11 +148,6 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
-
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
